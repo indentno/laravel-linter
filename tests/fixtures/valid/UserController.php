38b401ab1d0126c973033cebd45d97b492aaa073
Runar Jørgensen <ventrec@ventrec.org>
2021-03-08 12:31:09 +0100

    Adds test for ControllerHasCorrectOrderForRestMethods linter. Adds new fixture and updates existing fixtures.

--- tests/fixtures/valid/UserController.php
+++ tests/fixtures/valid/UserController.php
@@ -8,8 +8,29 @@ class UserController
 {
     public function index()
     {
-        $users = User::get();
+        $users = $this->getUsers();
 
         return view('user.index', ['users' => $users]);
+    }
+
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        User::create([
+            'firstname' => $request->firstname,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return redirect()->back()->with('success_message', 'User successfully created.');
+    }
+
+    private function getUsers()
+    {
+        return User::get();
     }
 }
