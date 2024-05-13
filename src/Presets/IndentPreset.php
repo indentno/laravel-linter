5a1d764314149c7221b0a086b9bd2f9198c1ec6f
Runar Jørgensen <ventrec@ventrec.org>
2024-05-13 10:19:11 +0200

    Updates tlint version. Removes UseAuthHelperOverFacade linter. Updates "->parts" logic in order to comply with changes in api.

--- src/Presets/IndentPreset.php
+++ src/Presets/IndentPreset.php
@@ -21,7 +21,6 @@ use Tighten\TLint\Linters\RemoveLeadingSlashNamespaces;
 use Tighten\TLint\Linters\SpaceAfterBladeDirectives;
 use Tighten\TLint\Linters\SpacesAroundBladeRenderContent;
 use Tighten\TLint\Linters\UseAnonymousMigrations;
-use Tighten\TLint\Linters\UseAuthHelperOverFacade;
 use Tighten\TLint\Presets\PresetInterface;
 
 class IndentPreset implements PresetInterface
@@ -45,7 +44,6 @@ class IndentPreset implements PresetInterface
             ControllerHasCorrectOrderForRestMethods::class,
             SpaceAfterBladeDirectives::class,
             SpacesAroundBladeRenderContent::class,
-            UseAuthHelperOverFacade::class,
             UseConfigOverEnv::class,
             ValidRouteStructure::class,
             UseAnonymousMigrations::class,
